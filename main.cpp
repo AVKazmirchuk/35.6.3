f824dbd1985c58d39152871defe3581fcddc5f3b
AVKazmirchuk <85755971+AVKazmirchuk@users.noreply.github.com>
2021-11-21 18:41:05 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,47 +1,48 @@
-﻿#include <iostream>
+#include <iostream>
 #include <filesystem>
 #include <vector>
 #include <iterator>
+#include <string>
 #include <algorithm>
 
 int main()
 {
-	auto l{ [](const std::string& dir, const std::string& ext) {
+    auto l{ [](const std::string& dir, const std::string& ext) {
 
-		std::filesystem::path path(dir);
+        std::filesystem::path path(dir);
 
-		auto u_ptr = std::make_unique<std::vector<std::string>>();
+        auto u_ptr = std::make_unique<std::vector<std::string>>();
 
-		for (const auto& elem : std::filesystem::recursive_directory_iterator{ path }) {
-			if (std::filesystem::is_regular_file(std::filesystem::status(elem.path())) && elem.path().extension().string() == ext) 
-				std::back_inserter(*u_ptr) = elem.path().filename().string();
-		}
+        for (const auto& elem : std::filesystem::recursive_directory_iterator{ path }) {
+            if (std::filesystem::is_regular_file(std::filesystem::status(elem.path())) && elem.path().extension().string() == ext)
+                std::back_inserter(*u_ptr) = elem.path().filename().string();
+        }
 
-		return u_ptr;
-	}};
-		
-	std::string dir;
-		
-	while (true)
-	{
-		std::cout << "Path: ";
-		
-		std::cin >> dir;
+        return u_ptr;
+    }};
 
-		if (std::filesystem::exists(dir)) break;
+    std::string dir;
 
-		std::cout << "Path not found!\n";
-	}
+    while (true)
+    {
+        std::cout << "Path: ";
 
-	std::cout << "Extension: ";
-	std::string ext;
-	std::cin >> ext;
+        std::cin >> dir;
 
-	if (ext[0] != '.') ext = '.' + ext;
-	
-	auto u_ptr = l(dir, ext);
+        if (std::filesystem::exists(dir)) break;
 
-	std::cout << "\nList of files:\n";
+        std::cout << "Path not found!\n";
+    }
 
-	std::copy(u_ptr.get()->cbegin(), u_ptr.get()->cend(), std::ostream_iterator<std::string>(std::cout, "\n"));
+    std::cout << "Extension: ";
+    std::string ext;
+    std::cin >> ext;
+
+    if (ext[0] != '.') ext = '.' + ext;
+
+    auto u_ptr = l(dir, ext);
+
+    std::cout << "\nList of files:\n";
+
+    std::copy(u_ptr.get()->cbegin(), u_ptr.get()->cend(), std::ostream_iterator<std::string>(std::cout, "\n"));
 }
